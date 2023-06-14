aada24684d1fd4c01696cbff3850e714f9d15794
Souha <souhaila.rabai@gmail.com>
2023-06-14 11:06:42 +0200

    comments has been added to text_loader.cpp

--- text_loader.cpp
+++ text_loader.cpp
@@ -1,48 +1,51 @@
 /**
+ * @file text_loader.h
+ * @brief Définition de la classe text_loader.
+ */
 
-@file text_loader.cpp
-@brief Implementation of the text_loader class.
-*/
 #include "text_loader.h"
 #include "storage_type.h"
 #include <string>
 #include <fstream>
-#include <array>
+#include <vector>
 #include <iostream>
 
 using namespace std;
 
 /**
-
-@brief Loads data from a text file into the storage.
-
-This function reads each line from the specified text file
-
-and converts it to a numerical value, which is then stored
-
-in the provided storage container.
-
-@param storage The storage container to load the data into.
-
-@return The updated storage container with the loaded data.
-
-@throws std::ios_base::failure If the file cannot be opened.
-*/
-storage_type& text_loader::load(storage_type& storage)
+ * @brief Charge les données à partir d'un fichier texte et les stocke dans le conteneur.
+ *
+ * Cette classe permet de charger des données numériques à partir d'un fichier texte
+ * et de les stocker dans un conteneur fourni en argument.
+ */
+class text_loader
 {
-storage_type& returned_storage = storage;
-ifstream f(source);
+public:
+    /**
+     * @brief Charge les données à partir d'un fichier texte dans le conteneur fourni.
+     *
+     * @param storage Le conteneur où stocker les données.
+     * @return Une référence vers le conteneur mis à jour avec les données chargées.
+     * @throw ios_base::failure si le fichier ne peut pas être ouvert.
+     */
+    storage_type &load(storage_type &storage);
+};
 
-if (!f.is_open())
+storage_type &text_loader::load(storage_type &storage)
 {
-throw ios_base::failure("Can't open the file.");
-}
+    storage_type &returned_storage = storage;
+    ifstream file(source);  // Ouverture du fichier
 
-string line;
-while (getline(f, line))
-{
-returned_storage.push_back(stold(line));
-}
+    if (!file.is_open())
+    {
+        throw ios_base::failure("Can't open the file.");
+    }
 
-return returned_storage;
+    string line;
+    while (getline(file, line))
+    {
+        returned_storage.push_back(stold(line));  // Conversion de chaque ligne en nombre et ajout au conteneur
+    }
+
+    return returned_storage;
 }
