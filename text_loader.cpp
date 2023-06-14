505e08a599654c20c8c784bf50685b15358628aa
Souha <souhaila.rabai@gmail.com>
2023-06-14 11:13:17 +0200

    comments has been added to text_loader.cpp

--- text_loader.cpp
+++ text_loader.cpp
@@ -1,50 +1,37 @@
 /**
- * @file text_loader.h
- * @brief Définition de la classe text_loader.
+ * \file text_loader.h
+ * \brief Définition de la classe text_loader.
  */
 
 #include "text_loader.h"
 #include "storage_type.h"
 #include <string>
 #include <fstream>
-#include <vector>
+#include <array>
 #include <iostream>
 
 using namespace std;
 
 /**
- * @brief Charge les données à partir d'un fichier texte et les stocke dans le conteneur.
- *
- * Cette classe permet de charger des données numériques à partir d'un fichier texte
- * et de les stocker dans un conteneur fourni en argument.
+ * \brief Charge les données à partir d'un fichier texte dans une structure de stockage.
+ * \param storage La structure de stockage à remplir avec les données du fichier.
+ * \return La structure de stockage remplie.
+ * \throws ios_base::failure Si le fichier ne peut pas être ouvert.
  */
-class text_loader
-{
-public:
-    /**
-     * @brief Charge les données à partir d'un fichier texte dans le conteneur fourni.
-     *
-     * @param storage Le conteneur où stocker les données.
-     * @return Une référence vers le conteneur mis à jour avec les données chargées.
-     * @throw ios_base::failure si le fichier ne peut pas être ouvert.
-     */
-    storage_type &load(storage_type &storage);
-};
-
 storage_type &text_loader::load(storage_type &storage)
 {
     storage_type &returned_storage = storage;
-    ifstream file(source);  // Ouverture du fichier
+    ifstream f(source);
 
-    if (!file.is_open())
+    if (!f.is_open())
     {
         throw ios_base::failure("Can't open the file.");
     }
 
     string line;
-    while (getline(file, line))
+    while (getline(f, line))
     {
-        returned_storage.push_back(stold(line));  // Conversion de chaque ligne en nombre et ajout au conteneur
+        returned_storage.push_back(stold(line));
     }
 
     return returned_storage;
